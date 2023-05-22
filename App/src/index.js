15ef74b52d65d1146e640c3d1723df1c28b59792
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2023-05-22 14:32:24 +0700

    fix heroku

--- App/src/index.js
+++ App/src/index.js
@@ -51,4 +51,6 @@ app.get('/', function (req, res) {
   res.redirect('/');
 });
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
