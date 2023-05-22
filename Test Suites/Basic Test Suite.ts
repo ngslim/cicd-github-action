431503c36a9f353aed830e2b3bd2f6b67d1d8a9d
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2023-05-22 10:31:22 +0700

    add test suite

---
+++ Test Suites/Basic Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Basic Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>84d66931-b66b-4a70-a637-88c4aa96d6b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>399d2161-3513-43e6-993c-1894889afcf3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1 Log in</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
