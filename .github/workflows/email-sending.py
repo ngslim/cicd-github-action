e3b71d03ccb1c2ad49836feedf60204ad3a42355
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2023-05-27 13:46:36 +0700

    fix

---
+++ .github/workflows/email-sending.py
@@ -0,0 +1,21 @@
+import smtplib, ssl
+import os
+
+port = 465
+smtp_server = "smtp.gmail.com"
+USER_EMAIL = os.environ.get("USER_EMAIL")
+USER_PASSWORD = os.environ.get("USER_PASSWORD")
+
+
+message = """\
+    Subject: Test failed
+
+    Test failed
+"""
+
+context = ssl.create_default_context()
+
+server = smtplib.SMTP_SSL(smtp_server, port, context=context)
+
+server.login(USER_EMAIL, USER_PASSWORD)
+server.sendmail("19120272@student.hcmus.edu.vn", USER_EMAIL, message)
