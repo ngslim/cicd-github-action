92a3534ea2b0322f170e51297aaae17c8a16a1a9
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2023-05-27 11:50:44 +0700

    add test case

--- Test Suites/Basic Test Suite.ts
+++ Test Suites/Basic Test Suite.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>84d66931-b66b-4a70-a637-88c4aa96d6b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>e0199bcc-d2ad-4f99-ba0e-1a75a624cbbb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User/TC01_Get User Profile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
