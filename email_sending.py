b534fe15a2f570511d37a4c2679671f99b541fec
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2023-05-27 15:06:09 +0700

    fix

--- email_sending.py
+++ email_sending.py
@@ -3,19 +3,22 @@ import os
 
 port = 465
 smtp_server = "smtp.gmail.com"
+
 USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
 
+message = "Test failed."
 
-message = """\
-    Subject: Test failed
-
-    Test failed
-"""
+RECIPIENTS = []
+RECIPIENTS.append("ngggslim@gmail.com")
 
 context = ssl.create_default_context()
 
 server = smtplib.SMTP_SSL(smtp_server, port, context=context)
 
 server.login(USER_EMAIL, USER_PASSWORD)
-server.sendmail(USER_EMAIL, USER_EMAIL, USER_EMAIL, message)
+
+for recipient in RECIPIENTS:
+    header = 'To:' + recipient + '\n' + 'From: ' + USER_EMAIL + '\n' + 'Subject: TEST FAILED \n'
+    msg = header + message
+    server.sendmail(USER_EMAIL, recipient, msg)
